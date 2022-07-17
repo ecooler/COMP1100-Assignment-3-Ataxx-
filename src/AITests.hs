670cbdc99f899f5070064ea224b171bc748f574f
powcoder代写 <powcoder@163.com>
2022-07-17 18:58:41 +0800

    程序代写 加微信 powcoder Programming Help Add Wechat powcoder

---
+++ src/AITests.hs
@@ -0,0 +1,16 @@
+{-|
+Module      : AITests
+Description : Tests for your AI functions
+Copyright   : (c) 2020 Your Name Here
+License     : AllRightsReserved
+-}
+module AITests where
+
+import AI
+import Ataxx
+import Testing
+
+aiTests :: Test
+aiTests = TestGroup "AI"
+  [
+  ]
